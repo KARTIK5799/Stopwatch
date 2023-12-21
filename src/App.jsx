144c43ee86beaac224423197ecbfe834eb55fdd8
KARTIK5799 <thekartiksapkal@gmail.com>
2023-12-21 14:24:28 +0530

    Stopwatch

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Stopwatch from './Components/Stopwatch/Stopwatch'
+function App() {
+
+
+  return (
+    <div className="bg-red-700">
+  <Stopwatch />
+</div>
+  )
+}
+
+export default App
